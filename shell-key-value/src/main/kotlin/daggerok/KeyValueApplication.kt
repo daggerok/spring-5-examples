b4a45fe1a58ce9482d48852aeb043299226087aa
Maksim Kostromin <daggerok@gmail.com>
2017-11-04 23:02:17 +0200

    update shell app

--- shell-key-value/src/main/kotlin/daggerok/KeyValueApplication.kt
+++ shell-key-value/src/main/kotlin/daggerok/KeyValueApplication.kt
@@ -1,6 +1,6 @@
 package daggerok
 
-import org.springframework.boot.ApplicationRunner
+import org.springframework.beans.factory.InitializingBean
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
@@ -8,13 +8,11 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.keyvalue.annotation.KeySpace
 import org.springframework.data.map.repository.config.EnableMapRepositories
 import org.springframework.data.repository.CrudRepository
-import org.springframework.http.MediaType.APPLICATION_JSON_UTF8
+import org.springframework.shell.standard.ShellComponent
+import org.springframework.shell.standard.ShellMethod
 import org.springframework.stereotype.Repository
-import org.springframework.web.reactive.function.server.ServerRequest
-import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.body
-import org.springframework.web.reactive.function.server.router
-import reactor.core.publisher.Flux
+import org.springframework.stereotype.Service
+import java.util.concurrent.atomic.AtomicBoolean
 import java.util.stream.Stream
 
 @KeySpace("user")
@@ -24,26 +22,39 @@ data class User(@Id var id: String? = null,
 @Repository
 interface UserRepository : CrudRepository<User, String>
 
+@Service
+class ConnectionService {
+
+  val connected = AtomicBoolean()
+
+  fun isConnected() = connected.get()
+  fun connect() {
+    connected.set(true)
+  }
+
+  fun disconnect() {
+    connected.set(false)
+  }
+}
+
+@ShellComponent
+class ConnectionCommands(val svc: ConnectionService) {
+
+  @ShellMethod("connect to the system `connect --name <your name>`")
+  fun connect(name: String) = svc.connect()
+}
+
 @SpringBootApplication
 @EnableMapRepositories(basePackageClasses = arrayOf(UserRepository::class))
 class KeyValueApplication {
 
   @Bean
-  fun initializer(repo: UserRepository) = ApplicationRunner {
+  fun initializer(repo: UserRepository) = InitializingBean {
     Stream.of("max", "dag", "daggerok")
-          .map { User(null, it.capitalize(), it) }
-          .forEach { repo.save(it) }
-  }
+        .map { User(null, it.capitalize(), it) }
+        .forEach { repo.save(it) }
 
-  @Bean
-  fun routes(repo: UserRepository) = router {
-
-    fun getData(@Suppress("UNUSED_PARAMETER") req: ServerRequest) =
-        ServerResponse.ok().body(Flux.fromIterable(repo.findAll()))
-
-    (accept(APPLICATION_JSON_UTF8) and "/").nest {
-      GET("/**", ::getData)
-    }
+    repo.findAll().forEach { println(it) }
   }
 }
 
