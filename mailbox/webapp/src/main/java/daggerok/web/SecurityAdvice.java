3080fe8e2e148b259122002d9330655652f03ede
Maksim Kostromin <daggerok@gmail.com>
2018-01-17 09:51:30 +0200

    revert back to worked state

--- mailbox/webapp/src/main/java/daggerok/web/SecurityAdvice.java
+++ mailbox/webapp/src/main/java/daggerok/web/SecurityAdvice.java
@@ -3,13 +3,14 @@ package daggerok.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.DefaultCsrfToken;
-import org.springframework.security.web.reactive.result.view.CsrfRequestDataValueProcessor;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.security.Principal;
+
+import static org.springframework.security.web.reactive.result.view.CsrfRequestDataValueProcessor.DEFAULT_CSRF_ATTR_NAME;
 
 @Slf4j
 @ControllerAdvice
@@ -19,17 +20,14 @@ public class SecurityAdvice {
 
   @ModelAttribute("csrfToken") Mono<CsrfToken> csrfToken(final ServerWebExchange exchange) {
 
-    exchange.getAttributes().forEach((k, v) -> log.info("k={}, v={}", k, v));
+    //exchange.getAttributes().forEach((k, v) -> log.info("k={}, v={}", k, v));
 
     final CsrfToken csrfToken = exchange.getAttribute(CsrfToken.class.getName());
 
     return Mono.justOrEmpty(csrfToken)
-               .switchIfEmpty(Mono.just(new DefaultCsrfToken(CsrfRequestDataValueProcessor.DEFAULT_CSRF_ATTR_NAME,
-                                                             CsrfRequestDataValueProcessor.DEFAULT_CSRF_ATTR_NAME,
-                                                             "empty"))
+               .switchIfEmpty(Mono.just(new DefaultCsrfToken("CSRF_TOKEN", DEFAULT_CSRF_ATTR_NAME, "empty"))
                                   .doOnSuccess(token -> exchange.getAttributes()
-                                                                .put(CsrfRequestDataValueProcessor.DEFAULT_CSRF_ATTR_NAME,
-                                                                     token)));
+                                                                .put(DEFAULT_CSRF_ATTR_NAME, token)));
   }
 
   @ModelAttribute("currentUser") Mono<Principal> currentUser(final Mono<Principal> currentUser) {
--- mailbox/webapp/src/main/java/daggerok/web/SecurityConfig.java
+++ mailbox/webapp/src/main/java/daggerok/web/SecurityConfig.java
@@ -2,6 +2,8 @@ package daggerok.web;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
@@ -10,20 +12,25 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
 public class SecurityConfig {
 
   @Bean SecurityWebFilterChain springSecurityFilterChain(final ServerHttpSecurity http) {
 
     http
         .authorizeExchange()
-          .pathMatchers("/favicon.ico")
+          .pathMatchers("/favicon.ico", "/css/**", "/webjars/**")
             .permitAll()
           .anyExchange()
             .authenticated()
             .and()
         .httpBasic()
           .and()
-        .formLogin();
+        .formLogin()
+          .and()
+        .logout()
+    ;
 
     return http.build();
   }
