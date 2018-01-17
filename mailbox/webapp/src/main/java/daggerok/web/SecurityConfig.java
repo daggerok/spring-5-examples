60ced79f5ae4f2c10106cb460847032d469e8020
Maksim Kostromin <daggerok@gmail.com>
2018-01-17 08:37:13 +0200

    add custom http security

--- mailbox/webapp/src/main/java/daggerok/web/SecurityConfig.java
+++ mailbox/webapp/src/main/java/daggerok/web/SecurityConfig.java
@@ -2,13 +2,31 @@ package daggerok.web;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+  @Bean SecurityWebFilterChain springSecurityFilterChain(final ServerHttpSecurity http) {
+
+    http
+        .authorizeExchange()
+          .pathMatchers("/favicon.ico")
+            .permitAll()
+          .anyExchange()
+            .authenticated()
+            .and()
+        .httpBasic()
+          .and()
+        .formLogin();
+
+    return http.build();
+  }
 
   @Bean MapReactiveUserDetailsService userDetailsService() {
     return new MapReactiveUserDetailsService(
