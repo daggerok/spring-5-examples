282f3dd5caa05d86e590bb4b1a36be400661d046
Maksim Kostromin <daggerok@gmail.com>
2018-02-17 21:51:23 +0200

    add Github WebClinet token intercepter

---
+++ github-api-v3-gateway/webflux-gateway/src/main/java/daggerok/github/WebClientConfig.java
@@ -0,0 +1,35 @@
+package daggerok.github;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.Base64Utils;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import javax.validation.constraints.Pattern;
+import java.nio.charset.StandardCharsets;
+
+import static java.lang.String.format;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Configuration
+public class WebClientConfig {
+
+  @Bean
+  WebClient githubWebClient(final GithubProperties props) {
+    return WebClient.builder()
+                    .filter((request, next) -> next.exchange(
+                        ClientRequest.from(request)
+                                     .header(AUTHORIZATION, basicAuthorization(props.getToken()))
+                                     .build()))
+                    .build();
+  }
+
+  private static String basicAuthorization(final @Pattern(regexp = "\\w+:\\w+") String token) {
+
+    final byte[] basicAuthValue = token.getBytes(StandardCharsets.UTF_8);
+    final String encoded = Base64Utils.encodeToString(basicAuthValue);
+
+    return format("Basic %s", encoded);
+  }
+}
