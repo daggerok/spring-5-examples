472c7c70768059387f54f095ff4e04645a4f3d65
Maksim Kostromin <daggerok@gmail.com>
2018-02-17 20:07:43 +0200

    try fix secured travis env

--- github-api-v3-gateway/webflux-gateway/src/main/java/daggerok/github/GithubProperties.java
+++ github-api-v3-gateway/webflux-gateway/src/main/java/daggerok/github/GithubProperties.java
@@ -1,16 +1,34 @@
 package daggerok.github;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.Pattern;
+
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
+import static com.fasterxml.jackson.annotation.JsonProperty.Access.READ_ONLY;
 
 @Getter
 @Setter
+@Validated
+@JsonAutoDetect(
+    fieldVisibility = NONE,
+    setterVisibility = NONE,
+    getterVisibility = NONE,
+    isGetterVisibility = NONE,
+    creatorVisibility = NONE
+)
 @ConfigurationProperties(prefix = "github")
 public class GithubProperties {
 
   /**
    * github token, see:
    */
+  @JsonProperty(access = READ_ONLY)
+  @Pattern(regexp = "\\w+:\\w+")
   String token;
 }
--- github-api-v3-gateway/webflux-gateway/src/main/java/daggerok/github/GithubPropertiesRoutes.java
+++ github-api-v3-gateway/webflux-gateway/src/main/java/daggerok/github/GithubPropertiesRoutes.java
@@ -1,6 +1,5 @@
 package daggerok.github;
 
-import io.vavr.collection.HashMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -9,6 +8,7 @@ import reactor.core.publisher.Mono;
 
 import java.util.Map;
 
+import static java.util.Collections.singletonMap;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
@@ -21,12 +21,17 @@ public class GithubPropertiesRoutes {
 
     return
 
-        route(GET("/github/**"),
-              request -> ok().body(Mono.just(props), GithubProperties.class))
+        route(GET("/github/manual"),
+              request -> ok().body(Mono.just(
+                  singletonMap("github",
+                               singletonMap("token", props.getToken()))),
+                                   Map.class))
+
+            .andRoute(GET("/github/**"),
+                      request -> ok().body(Mono.just(props), GithubProperties.class))
 
             .andRoute(GET("/**"),
-                      request -> ok().body(Mono.just(HashMap.of("result", "TODO")
-                                                            .toJavaMap()),
+                      request -> ok().body(Mono.just(singletonMap("result", "TODO")),
                                            Map.class))
 
         ;
